2322ec4d6750e417d1a20c7ce4744732c7e8594d
Saad Nadeem <xaad46@gmail.com>
2022-04-05 18:48:22 +0500

    Added Header

--- src/components/FormPage/FormPage.tsx
+++ src/components/FormPage/FormPage.tsx
@@ -4,18 +4,51 @@ import Typography from '@mui/material/Typography';
 
 import { styled } from '@mui/system';
 
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
+import NetworkWifiIcon from '@mui/icons-material/NetworkWifi';
+import Battery6BarIcon from '@mui/icons-material/Battery6Bar';
+
 const MainContainer = styled(Container)({
     color: 'darkslategray',
     backgroundColor: 'aliceblue',
     padding: '0.5rem',
+    display: 'flex',
 });
+
+const Header = styled(Container)({
+    color: 'darkslategray',
+    backgroundColor: 'aliceblue',
+    padding: '0.5rem',
+    display: 'flex',
+});
+
+const IconsContainer = styled(Container)({
+    color: 'darkslategray',
+    backgroundColor: 'aliceblue',
+    display: 'flex',
+    justifyContent: 'end',
+    gap: '0.2rem',
+    width: '100%',
+});
+
+const Date = styled(Typography)({
+    whiteSpace: 'nowrap',
+    fontWeight: 'bold',
+})
 
 export default function FormPage() {
     return (
         <MainContainer>
-            <Typography>
-                9:41 AM
-            </Typography>
+            <Header>
+                <Date>
+                    9:41 AM
+                </Date>
+                <IconsContainer>
+                    <SignalCellularAltIcon />
+                    <NetworkWifiIcon />
+                    <Battery6BarIcon />
+                </IconsContainer>
+            </Header>
         </MainContainer>
     )
 }
