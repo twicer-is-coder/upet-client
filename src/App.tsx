5add858be29c84ca7a3ab9ac0410651616ba62f3
Saad Nadeem <xaad46@gmail.com>
2022-04-05 18:24:44 +0500

    Added Styled Component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import FormPage from "./components/FormPage/FormPage";
 import ThankYouPage from "./components/ThankYouPage/ThankYouPage"
-
 import './App.css';
+
 function App() {
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<FormPage />} />
-        <Route path="thankyou" element={<ThankYouPage />} />
-      </Routes>
-    </div>
+      <BrowserRouter>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<FormPage />} />
+            <Route path="thankyou" element={<ThankYouPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
--- src/components/FormPage/FormPage.tsx
+++ src/components/FormPage/FormPage.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Container } from '@mui/material'
+import Typography from '@mui/material/Typography';
+
+import { styled } from '@mui/system';
+
+const MainContainer = styled(Container)({
+    color: 'darkslategray',
+    backgroundColor: 'aliceblue',
+    padding: '0.5rem',
+});
 
 export default function FormPage() {
     return (
-        <div>
-            FormPage
-        </div>
+        <MainContainer>
+            <Typography>
+                9:41 AM
+            </Typography>
+        </MainContainer>
     )
 }
