3aebeab867da1bc2ab6745032395c2d849b451f6
Saad Nadeem <xaad46@gmail.com>
2022-04-05 17:21:43 +0500

    Created Base.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
+import FormPage from "./components/FormPage/FormPage";
+import ThankYouPage from "./components/ThankYouPage/ThankYouPage"
 
-import logo from './logo.svg';
 import './App.css';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>} />
-      <Route path="about" element={<h1>About Us</h1>} />
+      <Route path="/" element={<FormPage />} />
+      <Route path="thankyou" element={<ThankYouPage />} />
     </Routes>
   );
 }
---
+++ src/components/FormPage/FormPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FormPage() {
+    return (
+        <div>
+            FormPage
+        </div>
+    )
+}
